c63e806247655a2c3b075cb98621567572889158
sanmiranda <sanmiranda@hotmail.com>
2018-12-20 08:29:14 -0600

    models and CRUD

--- rekofront/src/components/auth/Profile.jsx
+++ rekofront/src/components/auth/Profile.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+
+
+const displayAvartar = (user) => {
+  if(user.avatarUrl){
+    return <img src={user.avatarUrl} />
+  } else {
+    return <img src='https://s3.amazonaws.com/owler-image/logo/ironhack_owler_20180828_221413_original.png' width='300' height='300'/>
+  }
+}
+
+
 export default function Profile() {
   return (
     <div>
